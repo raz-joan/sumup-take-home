4122847f378405e725c8eaa1224b8ab7617bcd2e
Joan Rasmussen <raz.joan@gmail.com>
2022-03-05 11:47:05 -0700

    Extract and transform data by client

--- transactions.js
+++ transactions.js
@@ -4,11 +4,30 @@ const csv = require('csv-parser')
 const clients = []
 
 fs.createReadStream('transactions.csv')
-  .pipe(csv({}))
-  .on('data', (data) => {
-    clients.push(data)
+  .pipe(csv())
+  .on('data', (row) => {
+    const foundClientIndex = clients.findIndex(each => each.client === Number(row.client))
+
+    if (foundClientIndex < 0) {
+      const user = {
+        client: Number(row.client),
+        transactions: [{
+          type: row.type,
+          tx: Number(row.tx),
+          amount: Number(row.amount).toFixed(4)
+        }]
+      }
+      clients.push(user)
+    } else {
+      clients[foundClientIndex].transactions.push({
+        type: row.type,
+        tx: Number(row.tx),
+        amount: Number(row.amount).toFixed(4)
+      })
+    }
   })
   .on('end', () => {
     console.table(clients)
     console.log(clients)
+    console.log(clients[0].transactions)
   })
