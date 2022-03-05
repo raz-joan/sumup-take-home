e9b55298509642bf0f9bd09950c7b4df6ddba430
Joan Rasmussen <raz.joan@gmail.com>
2022-03-05 12:20:53 -0700

    Write skeleton switch for calculateBalances()

--- transactions.js
+++ transactions.js
@@ -3,6 +3,43 @@ const csv = require('csv-parser')
 
 const clients = []
 
+const calculateBalances = () => {
+  clients.map(each => {
+    console.log(each.client)
+    console.log(each.transactions)
+
+    const transformedClient = {
+      client: each.client,
+      available: 0,
+      held: 0,
+      total: 0,
+      locked: false
+    }
+
+    each.transactions.forEach(transaction => {
+      switch (transaction.type) {
+        case 'deposit':
+          console.log(each.client, 'deposit')
+          break
+        case 'withdrawal':
+          console.log(each.client, 'withdrawal')
+          break
+        case 'dispute':
+          console.log(each.client, 'dispute')
+          break
+        case 'resolve':
+          console.log(each.client, 'resolve')
+          break
+        case 'chargeback':
+          console.log(each.client, 'chargeback')
+          break
+        default:
+          console.log(`error, no match found for ${transaction.type}`)
+      }
+    })
+  })
+}
+
 fs.createReadStream('transactions.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -27,7 +64,8 @@ fs.createReadStream('transactions.csv')
     }
   })
   .on('end', () => {
-    console.table(clients)
+    // console.table(clients)
     console.log(clients)
-    console.log(clients[0].transactions)
+    // console.log(clients[0].transactions)
+    calculateBalances()
   })
